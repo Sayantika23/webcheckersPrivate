4431c5ddf0fdf04fe9cb171cd4080853946b13d7
Hiten1928 <31774329+Hiten1928@users.noreply.github.com>
2017-10-16 12:10:34 -0400

    Add files via upload

---
+++ Piece.java
@@ -0,0 +1,32 @@
+package com.webcheckers.model;
+
+
+/**
+ *
+ *  Shows the type of piece on the borad for the CheckersGame
+ * @author Sankarsh,Hiten,Girish,Fasaiel
+ */
+public class Piece {
+
+    public TYPE type;
+    public COLOR color;
+
+    public enum TYPE {SINGLE, KING}
+    public  enum COLOR {RED, WHITE}
+
+    public TYPE getType() {
+        return type;
+    }
+
+    public void setType(TYPE type) {
+        this.type = type;
+    }
+
+    public COLOR getColor() {
+        return color;
+    }
+
+    public void setColor(COLOR color) {
+        this.color = color;
+    }
+}
