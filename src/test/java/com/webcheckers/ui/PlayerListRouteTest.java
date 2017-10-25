1fca90492b89ba3f640abf7eed81413163b2213f
Hiten1928 <31774329+Hiten1928@users.noreply.github.com>
2017-10-25 14:24:55 -0400

    Girish's file added to master

--- src/test/java/com/webcheckers/ui/PlayerListRouteTest.java
+++ src/test/java/com/webcheckers/ui/PlayerListRouteTest.java
@@ -1,19 +1,51 @@
-package com.webcheckers.ui;
+package com.wecheckers.ui;
+
+import static org.junit.Assert.*;
+
+import org.mockito.Mockito;
+
+import java.util.*;
+
+import com.webcheckers.ui.PlayerListRoute;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import spark.*;
 
+/** Unit Test for the PlayerListRoute Component
+ *  @author Girish Kumar
+ */
 public class PlayerListRouteTest {
 
+    private Request request;
+    private Response response;
+    private ModelAndView modelAndView;
+    private Map<String,Object> vm;
+
+    private PlayerListRoute CuT = new PlayerListRoute();
 
     @Before
     public void setUp() throws Exception {
-    }
 
+        request = Mockito.mock(Request.class);
+        response = Mockito.mock(Response.class);
+        modelAndView = Mockito.mock(ModelAndView.class);
+        vm = Mockito.mock(HashMap.class);
+    }
     @Test
     public void handle() throws Exception {
+
+        Mockito.when(modelAndView.getViewName()).thenReturn("playerlist.ftl");
+
+        ModelAndView result = CuT.handle(request,response);
+
+        Map<String,Object> vm = (Map<String,Object>) result.getModel();
+
+        assertEquals(modelAndView.getViewName(),result.getViewName());
+
     }
 
-}
+    }
+
+
